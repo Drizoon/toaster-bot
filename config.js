46fbcc49a0da5888aaf2d896bb2d72bc7d112f48
Niosver <raladnier@hotmail.com>
2019-02-24 12:13:46 -0600

    Chnaged startup channel

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ let moderators = [
 ];
 
 // The bot will post a "I am running"-style message to this channel on startup.
-const startupChannel = 'Niosver';
+const startupChannel = 'cailighto';
 
 // if a channel is offline-only protected, and a change occurs, the bot prints
 // to this channel instead of the channel the change occurred in.
